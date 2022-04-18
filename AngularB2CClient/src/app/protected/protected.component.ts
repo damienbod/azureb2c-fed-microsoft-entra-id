fa74d9bae74c9db00e00f2a4107f141ae92fd1ad
damienbod <damien_bod@hotmail.com>
2022-04-18 19:14:05 +0200

    Add an API call

--- AngularB2CClient/src/app/protected/protected.component.ts
+++ AngularB2CClient/src/app/protected/protected.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-protected',
@@ -6,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./protected.component.css'],
 })
 export class ProtectedComponent implements OnInit {
-  constructor() {}
+  dataFromAzureProtectedApi$: Observable<any>;
+  isAuthenticated = false;
+  constructor(
+    private oidcSecurityService: OidcSecurityService,
+    private httpClient: HttpClient
+  ) {
+    this.dataFromAzureProtectedApi$ = of(null);
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.oidcSecurityService.isAuthenticated().subscribe(isAuthenticated => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
+
+  callApi() {
+    this.dataFromAzureProtectedApi$ = this.httpClient
+      .get('https://localhost:44395/UserAccess')
+      .pipe(catchError((error) => of(error)));
+  }
 }
+
+
