e5eda012b356b25f844e04b1cb6b8495b9e534e4
damienbod <damien_bod@hotmail.com>
2022-05-06 20:49:53 +0200

    clean up

--- AzureB2CUI/Controller/AccountSignUpController.cs
+++ AzureB2CUI/Controller/AccountSignUpController.cs
@@ -3,33 +3,32 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Microsoft.Identity.Web.UI.Areas.MicrosoftIdentity.Controllers
+namespace Microsoft.Identity.Web.UI.Areas.MicrosoftIdentity.Controllers;
+
+[AllowAnonymous]
+[Route("MicrosoftIdentity/[controller]/[action]")]
+public class AccountSignUpController : Controller
 {
-    [AllowAnonymous]
-    [Route("MicrosoftIdentity/[controller]/[action]")]
-    public class AccountSignUpController : Controller
+    [HttpGet("{scheme?}")]
+    public IActionResult SignUpPolicy(
+        [FromRoute] string scheme,
+        [FromQuery] string redirectUri)
     {
-        [HttpGet("{scheme?}")]
-        public IActionResult SignUpPolicy(
-            [FromRoute] string scheme,
-            [FromQuery] string redirectUri)
+        scheme ??= OpenIdConnectDefaults.AuthenticationScheme;
+        string redirect;
+        if (!string.IsNullOrEmpty(redirectUri) && Url.IsLocalUrl(redirectUri))
         {
-            scheme ??= OpenIdConnectDefaults.AuthenticationScheme;
-            string redirect;
-            if (!string.IsNullOrEmpty(redirectUri) && Url.IsLocalUrl(redirectUri))
-            {
-                redirect = redirectUri;
-            }
-            else
-            {
-                redirect = Url.Content("~/")!;
-            }
-
-            scheme ??= OpenIdConnectDefaults.AuthenticationScheme;
-
-            var properties = new AuthenticationProperties { RedirectUri = redirect };
-            properties.Items[Constants.Policy] = "B2C_1_signup";
-            return Challenge(properties, scheme);
+            redirect = redirectUri;
         }
+        else
+        {
+            redirect = Url.Content("~/")!;
+        }
+
+        scheme ??= OpenIdConnectDefaults.AuthenticationScheme;
+
+        var properties = new AuthenticationProperties { RedirectUri = redirect };
+        properties.Items[Constants.Policy] = "B2C_1_signup";
+        return Challenge(properties, scheme);
     }
 }
