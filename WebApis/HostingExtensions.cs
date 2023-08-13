a7e14bd5e022ee7732233860e364ca60d990689e
damienbod <damien_bod@hotmail.com>
2023-08-13 14:18:39 +0200

    Updated start up of API project

---
+++ WebApis/HostingExtensions.cs
@@ -0,0 +1,147 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.IdentityModel.Tokens.Jwt;
+using WebApis.Authz;
+
+namespace WebApis;
+
+internal static class HostingExtensions
+{
+    private static IWebHostEnvironment? _env;
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+        _env = builder.Environment;
+
+        services.AddHttpClient();
+
+        services.AddOptions();
+
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+        IdentityModelEventSource.ShowPII = true;
+
+        services.AddMicrosoftIdentityWebApiAuthentication(
+            configuration, "AzureB2CUserApi");
+        services.AddMicrosoftIdentityWebApiAuthentication(
+            configuration, "AzureB2CAdminApi", "BearerAdmin");
+
+        services.AddMicrosoftIdentityWebApiAuthentication(
+           configuration, "AzureB2CAngularApi", "BearerAngularApi");
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowMyOrigins",
+                builder =>
+                {
+                    builder
+                        .AllowCredentials()
+                        .WithOrigins(
+                            "https://localhost:4200")
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            // add JWT Authentication
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "JWT Authentication",
+                Description = "Enter JWT Bearer token **_only_**",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer", // must be lower case
+                BearerFormat = "JWT",
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+            c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {securityScheme, Array.Empty<string>()}
+            });
+
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Web APIs",
+                Version = "v1",
+                Description = "Web APIs",
+                Contact = new OpenApiContact
+                {
+                    Name = "damienbod",
+                    Email = string.Empty,
+                    Url = new Uri("https://damienbod.com/"),
+                },
+            });
+        });
+
+        services.AddControllers(options =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                // .RequireClaim("email") // disabled this to test with users that have no email (no license added)
+                .Build();
+            options.Filters.Add(new AuthorizeFilter(policy));
+        });
+
+        services.AddSingleton<IAuthorizationHandler, IsAdminHandlerUsingIdp>();
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("IsAdminRequirementPolicy", policyIsAdminRequirement =>
+            {
+                policyIsAdminRequirement.Requirements.Add(new IsAdminRequirement());
+            });
+        });
+
+        return builder.Build();
+    }
+    
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        IdentityModelEventSource.ShowPII = true;
+
+        app.UseSerilogRequestLogging();
+
+        if (_env!.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "User API One");
+            c.RoutePrefix = string.Empty;
+        });
+
+        app.UseCors("AllowMyOrigins");
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
